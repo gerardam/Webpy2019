fb57af8fa4df1a3b8fd7d86ed834b24a3433117d
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2019-07-26 21:22:23 -0500

    Agregado de archivo eliminar solicitud con clases

--- apps/adopcion/views.py
+++ apps/adopcion/views.py
@@ -45,8 +45,33 @@ class SolicitudUpdate(UpdateView):
     template_name = 'adopcion/solicitud_form.html'
     form_class = SolicitudForm
     second_form_class = PersonaForm
+    success_url = reverse_lazy('adopcion:lista_solicitud')
 
-
+    def get_context_data(self, **kwargs):
+        context = super(SolicitudUpdate, self).get_context_data(**kwargs)
+        pk = self.kwargs.get('pk', 0)
+        solicitud = self.model.objects.get(id = pk)
+        persona = self.second_model.objects.get(id = solicitud.persona_id)
+        if 'form' not in context:
+            context['form'] = self.form_class()
+        if 'form2' not in context:
+            context['form2'] = self.second_form_class(instance = persona)
+        context['id'] = pk
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object
+        id_solicitud = kwargs['pk']
+        solicitud = self.model.objects.get(id = id_solicitud)
+        persona = self.second_model.objects.get(id = solicitud.persona_id)
+        form = self.form_class(request.POST, instance = solicitud)
+        form2 = self.second_form_class(request.POST, instance = persona)
+        if form.is_valid() and form2.is_valid():
+            form.save()
+            form2.save()
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            return HttpResponseRedirect(self.get_success_url())
 
 class SolicitudDelete(DeleteView):
     model = Solicitud
