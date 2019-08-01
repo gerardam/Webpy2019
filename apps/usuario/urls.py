479d9e29e8528c3ec1976b47236987e59a67b82d
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2019-08-01 11:25:57 -0500

    crear json de usuarios con djangorestframework

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from apps.usuario.views import RegistroUsuario
+from apps.usuario.views import RegistroUsuario, UserAPI
 
 urlpatterns = [
     path('registrar_usuario/', RegistroUsuario.as_view(), name='registrar_usuario'),
+    path('api', UserAPI.as_view(), name='api'),
 ]
--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,11 +1,23 @@
+from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from apps.usuario.forms import RegistroForm
+from rest_framework.views import APIView
+from apps.usuario.serializers import UserSerializer
+import json
 
 class RegistroUsuario(CreateView):
     model = User
     template_name = 'usuario/registrar.html'
     form_class = RegistroForm
     success_url = reverse_lazy('mascota:lista_mascota')
+
+class UserAPI(APIView):
+    serializer = UserSerializer
+
+    def get(self, request, format=None):
+        lista = User.objects.all()
+        response = self.serializer(lista, many=True)
+        return HttpResponse(json.dumps(response.data), content_type='application/json')
